2ebcf7d671fe63ac316112d4f23392db0ebec094
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2023-02-17 14:23:59 +1100

    Add marker selection script

---
+++ scripts/marker_selection_hgdp_1kg.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+"""Pipeline for choosing markers for HGDP/1kG datasets"""
+
+from cpg_utils.hail_batch import init_batch, output_path
+import hail as hl
+
+HGDP = 'gs://cpg-hgdp-test/vds/1-0.vds'
+
+ONEKG = 'gs://cpg-thousand-genomes-test/vds/1-0.vds'
+
+NUM_ROWS_BEFORE_LD_PRUNE = 200000
+
+
+def main():
+    """choosing markers for HGDP/1kG datasets according to gnomAD"""
+
+    init_batch()
+
+    hgdp_vds = hl.vds.read_vds(HGDP)
+    onekg_vds = hl.vds.read_vds(ONEKG)
+
+    # Add GT field and make dense MTs
+    hgdp_gt = hgdp_vds.variant_data.transmute_entries(GT = hl.vds.lgt_to_gt(hgdp_vds.variant_data.LGT, hgdp_vds.variant_data.LA))
+    onekg_gt = onekg_vds.variant_data.transmute_entries(GT = hl.vds.lgt_to_gt(onekg_vds.variant_data.LGT, onekg_vds.variant_data.LA))
+    hgdp_mt = hl.vds.to_dense_mt(hl.vds.VariantDataset(hgdp_vds.reference_data, hgdp_gt))
+    onekg_mt = hl.vds.to_dense_mt(hl.vds.VariantDataset(onekg_vds.reference_data, onekg_gt))
+
+    # Join datasets
+    hgdp_1kg = hgdp_mt.union_cols(onekg_mt)
+
+    # run variant QC
+    hgdp_1kg = hl.variant_qc(hgdp_1kg)
+    # choose variants based off of gnomAD v3 parameters
+    # Inbreeding coefficient > -0.25 (no excess of heterozygotes)
+    # Must be single nucleotide variants that are autosomal (i.e., no sex), and bi-allelic
+    # Have an allele frequency above 0.1%
+    # Have a call rate above 99%
+    hgdp_1kg = hgdp_1kg.annotate_rows(
+        IB=hl.agg.inbreeding(
+            hgdp_1kg.GT, hgdp_1kg.variant_qc.AF[1]
+        )
+    )
+    hgdp_1kg = hgdp_1kg.filter_rows(
+        (hl.len(hgdp_1kg.alleles) == 2)
+        & (hgdp_1kg.locus.in_autosome())
+        & (hgdp_1kg.variant_qc.AF[1] > 0.01)
+        & (hgdp_1kg.variant_qc.call_rate > 0.99)
+        & (hgdp_1kg.IB.f_stat > -0.25)
+    )
+
+    # downsize input variants for ld_prune
+    # otherwise, persisting the pruned_variant_table will cause
+    # script to fail. See https://github.com/populationgenomics/ancestry/pull/79
+    hgdp_1kg = hgdp_1kg.cache()
+    nrows = hgdp_1kg.count_rows()
+    print(f'hgdp_1kg.count_rows() = {nrows}')
+    hgdp_1kg = hgdp_1kg.sample_rows(
+        NUM_ROWS_BEFORE_LD_PRUNE / nrows, seed=12345
+    )
+
+    # as per gnomAD, LD-prune variants with a cutoff of r2 = 0.1
+    pruned_variant_table = hl.ld_prune(
+        hgdp_1kg.GT, r2=0.1, bp_window_size=500000
+    )
+    hgdp_1kg = hgdp_1kg.filter_rows(
+        hl.is_defined(pruned_variant_table[hgdp_1kg.row_key])
+    )
+    mt_path = output_path('hgdp_1kg_filtered_variants.mt')
+    hgdp_1kg.write(mt_path)
+
+
+if __name__ == '__main__':
+    main()
